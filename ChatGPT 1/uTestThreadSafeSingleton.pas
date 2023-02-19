d16cb3cdb54dff5e591d88416c440961d262e42a
Ihor.Bielykh <instigator21@gmail.com>
2023-02-19 07:59:31 +0100

    New added methods, don't work

--- ChatGPT 1/uTestThreadSafeSingleton.pas
+++ ChatGPT 1/uTestThreadSafeSingleton.pas
@@ -16,9 +16,18 @@ type
     procedure TestGetInstance;
     procedure TestAddToken;
     procedure TestRemoveToken;
+    procedure TestTokenExists;
+    procedure TestGetTokenCount;
+    procedure TestContainsToken;
+    procedure TestAddDuplicateToken;
+    procedure TestRemoveNonExistentToken;
+    procedure TestAddAndRemoveTokensConcurrently;
   end;
 
 implementation
+
+uses
+  System.Classes;
 
 procedure TTestThreadSafeSingleton.SetUp;
 begin
@@ -49,6 +58,70 @@ begin
   FSingleton.AddToken('bar');
   FSingleton.RemoveToken('foo');
   CheckEquals(1, FSingleton.GetTokenCount);
+end;
+
+procedure TTestThreadSafeSingleton.TestTokenExists;
+begin
+  // Test checking if a token exists in the singleton's Tokens list
+  FSingleton.AddToken('foo');
+  CheckTrue(FSingleton.TokenExists('foo'));
+  CheckFalse(FSingleton.TokenExists('bar'));
+end;
+
+procedure TTestThreadSafeSingleton.TestGetTokenCount;
+begin
+  // Test getting the count of tokens in the singleton's Tokens list
+  FSingleton.AddToken('foo');
+  FSingleton.AddToken('bar');
+  CheckEquals(2, FSingleton.GetTokenCount);
+end;
+
+procedure TTestThreadSafeSingleton.TestContainsToken;
+begin
+  // Test checking if a token exists in the singleton's Tokens list using ContainsToken
+  FSingleton.AddToken('foo');
+  CheckTrue(FSingleton.ContainsToken('foo'));
+  CheckFalse(FSingleton.ContainsToken('bar'));
+end;
+
+procedure TTestThreadSafeSingleton.TestAddDuplicateToken;
+begin
+  // Test adding a duplicate token to the singleton's Tokens list
+  FSingleton.AddToken('foo');
+  FSingleton.AddToken('foo');
+  CheckEquals(1, FSingleton.GetTokenCount);
+end;
+
+procedure TTestThreadSafeSingleton.TestRemoveNonExistentToken;
+begin
+  // Test removing a token that does not exist in the singleton's Tokens list
+  FSingleton.AddToken('foo');
+  FSingleton.RemoveToken('bar');
+  CheckEquals(1, FSingleton.GetTokenCount);
+end;
+
+procedure TTestThreadSafeSingleton.TestAddAndRemoveTokensConcurrently;
+var
+  Thread1, Thread2: TThread;
+begin
+  // Test adding and removing tokens concurrently using two threads
+  Thread1 := TThread.CreateAnonymousThread(
+    procedure
+    begin
+      FSingleton.AddToken('foo');
+    end
+  );
+  Thread2 := TThread.CreateAnonymousThread(
+    procedure
+    begin
+      FSingleton.RemoveToken('foo');
+    end
+  );
+  Thread1.Start;
+  Thread2.Start;
+  Thread1.WaitFor;
+  Thread2.WaitFor;
+  CheckEquals(0, FSingleton.GetTokenCount);
 end;
 
 initialization
