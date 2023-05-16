9213161dfc44acd3c39cfeda610b314cb3400199
Ihor Bielykh <instigator21@gmail.com>
2023-05-17 01:59:29 +0200

    add DAO, refactoring, added recommended extension

--- FamilyAccountBot/src/bot.ts
+++ FamilyAccountBot/src/bot.ts
@@ -2,9 +2,11 @@ import TelegramBot, { Message } from 'node-telegram-bot-api';
 import { config } from './config';
 import { handleSpending, handleCalcCommand, handlePayoffCommand } from './messageHandlers';
 import { SQLiteDB } from './Database';
+import { UserDAO } from './UserDataAccessObject';
 
 const bot = new TelegramBot(config.TOKEN, { polling: true });
 const db = new SQLiteDB('database.db');
+const userDao = new UserDAO(db);
 
 (async () => {
   try {
@@ -12,9 +14,9 @@ const db = new SQLiteDB('database.db');
     // Now you can use other methods on the `database` instance, like `addUser()`, `getUserSpending()`, etc.
 
     bot.on('message', (msg: Message) => {
-      handleSpending(msg, db);
-      handleCalcCommand(msg, bot, db);
-      handlePayoffCommand(msg, bot, db);
+      handleSpending(msg, userDao);
+      handleCalcCommand(msg, bot, userDao);
+      handlePayoffCommand(msg, bot, userDao);
     });
 
   } catch (error) {
--- FamilyAccountBot/src/messageHandlers.ts
+++ FamilyAccountBot/src/messageHandlers.ts
@@ -1,25 +1,25 @@
 import TelegramBot, { Message } from 'node-telegram-bot-api';
 import { containsEuro, isPayoffCommand } from './utils';
-import { SQLiteDB } from './Database';
+import { IUserDAO } from './UserDataAccessObject';
 
-export async function handleSpending(msg: Message, db: SQLiteDB): Promise<void> {
+export async function handleSpending(msg: Message, userDao: IUserDAO): Promise<void> {
   if (msg.from?.first_name && msg.text?.match(/\d+/)) {
     const userId = msg.from.id.toString();
     const userName = msg.from.first_name;
-    const user = await db.getUserSpending(userId);
+    const user = await userDao.getUserSpending(userId);
     if (!user) {
-      await db.addUser(userId, userName);
+      await userDao.addUser(userId, userName);
     }
     const amount = parseFloat(msg.text.match(/\d+/)?.[0] || '0');
     const currency = containsEuro(msg.text) ? 'EUR' : 'UAH';
-    await db.updateUserSpending(userId, currency, amount);
+    await userDao.updateUserSpending(userId, currency, amount);
   }
 }
 
-export async function handleCalcCommand(msg: Message, bot: TelegramBot, db: SQLiteDB): Promise<void> {
+export async function handleCalcCommand(msg: Message, bot: TelegramBot, userDao: IUserDAO): Promise<void> {
   if (msg.text?.toLowerCase() === 'calc') {
     let response = '';
-    const users = await db.getAllUsers();
+    const users = await userDao.getAllUsers();
     for (const user of users) {
       response += `${user.name} spends ${user.spending_uah} UAH and ${user.spending_eur} EUR\n`;
     }
@@ -27,11 +27,11 @@ export async function handleCalcCommand(msg: Message, bot: TelegramBot, db: SQLi
   }
 }
 
-export async function handlePayoffCommand(msg: Message, bot: TelegramBot, db: SQLiteDB): Promise<void> {
+export async function handlePayoffCommand(msg: Message, bot: TelegramBot, userDao: IUserDAO): Promise<void> {
   if (isPayoffCommand(msg.text || '')) {
-    const users = await db.getAllUsers();
+    const users = await userDao.getAllUsers();
     for (const user of users) {
-      await db.resetUserSpending(user.id);
+      await userDao.resetUserSpending(user.id);
     }
     bot.sendMessage(msg.chat.id, `All spendings have been set to zero.`);
   }
